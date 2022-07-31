d8046351b27da55a6e204b11a12dcaf64f049cec
Alejandro Santana <alesantanaojeda@gmail.com>
2022-08-01 00:23:48 +0100

    KH-002 :: Refactor content.js

--- content.js
+++ content.js
@@ -5,16 +5,16 @@ window.onload = () => init();
 const init = () => {
     getCachedNodes().then(nodes => { cachedNodes = { ...nodes } });
     hoverInitialElements();
-    selectorOnchangeListener();
+    selectorsOnchangeListener();
 }
 
 async function getCachedNodes() {
     return new Promise((resolve, reject) => {
-        chrome.storage.sync.get('cachedNodes', (cacheNodes) => {
+        chrome.storage.sync.get('cachedNodes', (cachedNodes) => {
             if (chrome.runtime.lastError) {
                 return reject(chrome.runtime.lastError);
             }
-            resolve(cacheNodes);
+            resolve(cachedNodes);
         });
     });
 
@@ -46,41 +46,54 @@ const onAttach = (debuggerId) => {
     );
 }
 
-chrome.storage.onChanged.addListener((changes, area) => {
-    console.log(changes, area);
-    if (changes.selectors) {
+const selectorsOnchangeListener = () => {
+    chrome.storage.onChanged.addListener((changes, area) => {
+        if (!changes.selectors) {
+            return;
+        }
+
         const newKeys = Object.keys(changes.selectors.newValue ?? {});
         const oldKeys = Object.keys(changes.selectors.oldValue ?? {});
 
-        if (newKeys.length !== oldKeys.length) {
-            const addedSelector = added(newKeys, oldKeys);
-            const removedSelector = removed(newKeys, oldKeys);
+        (hasAddedOrRemovedSelector(newKeys, oldKeys))
+            ? handleAddedOrRemovedSelector(newKeys, oldKeys, changes)
+            : handleCheckedChange(changes);
+    });
+}
 
-            if (addedSelector) {
-                const selector = changes.selectors.newValue[addedSelector].value;
-                const element = document.querySelector(selector);
+const handleCheckedChange = (changes) => {
+    Object.entries(changes.selectors.newValue).forEach(([key, selector]) => {
+        const newValueChecked = selector.checked;
+        const oldValueChecked = changes.selectors.oldValue[key].checked;
 
-                cachedNodes[addedSelector] = element;
-                chrome.storage.sync.set({ cachedNodes });
-            }
-
-            if (removedSelector) {
-                replaceNode(changes.selectors.oldValue[removedSelector]);
-                delete cachedNodes[removedSelector];
-                chrome.storage.sync.set({ cachedNodes });
-            }
-        } else {
-            Object.entries(changes.selectors.newValue).forEach(([key, selector]) => {
-                const newValueChecked = selector.checked;
-                const oldValueChecked = changes.selectors.oldValue[key].checked;
-
-                if (!newValueChecked && oldValueChecked) {
-                    replaceNode(selector);
-                }
-            });
+        if (!newValueChecked && oldValueChecked) {
+            replaceNode(selector);
         }
+    });
+}
+
+const handleAddedOrRemovedSelector = (newKeys, oldKeys, changes) => {
+    const addedSelector = added(newKeys, oldKeys);
+    const removedSelector = removed(newKeys, oldKeys);
+
+    if (addedSelector) {
+        const selector = changes.selectors.newValue[addedSelector].value;
+        const element = document.querySelector(selector);
+
+        cachedNodes[addedSelector] = element;
+        chrome.storage.sync.set({ cachedNodes });
     }
-});
+
+    if (removedSelector) {
+        replaceNode(changes.selectors.oldValue[removedSelector]);
+        delete cachedNodes[removedSelector];
+        chrome.storage.sync.set({ cachedNodes });
+    }
+}
+
+const hasAddedOrRemovedSelector = (newKeys, oldKeys) => {
+    return newKeys.length !== oldKeys.length
+}
 
 function replaceNode(selector) {
     const element = document.querySelector(selector.value);
